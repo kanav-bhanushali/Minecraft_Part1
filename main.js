0e7eecb7e19200c5c25833c8d2683087cd89742d
kanav-bhanushali <71547881+kanav-bhanushali@users.noreply.github.com>
2022-03-21 16:51:53 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -32,7 +32,75 @@ function new_image(get_image) {
     });
 }
 
+window.addEventListener("keydown",my_keydown);
 
+function my_keydown(e){
+    keypressed=e.keyCode;
+    
+    if(e.shiftkey && keypressed=='80'){
+        block_image_width=block_image_width+10;
+        block_image_height=block_image_height+10;
+        document.getElementById("current_width").innerHTML=block_image_width;
+        document.getElementById("current_height").innerHTML=block_image_height;
+                }
 
+                if(e.shiftkey && keypressed=='77'){
+                    block_image_width=block_image_width-10;
+                    block_image_height=block_image_height-10;
+                    document.getElementById("current_width").innerHTML=block_image_width;
+                    document.getElementById("current_height").innerHTML=block_image_height;
+                }
+
+   if(keypressed == '38'){
+       up();
+   }
+   if(keypressed == '37'){
+    left();
+}
+
+if(keypressed == '40'){
+    down();
+}
+
+if(keypressed == '39'){
+    up();
+}
+
+if(keypressed == '87'){
+    new_image('wall.jpg');
+}
+
+if(keypressed == '71'){
+    new_image('ground.png');
+}
+
+if(keypressed == '76'){
+    new_image('light_green.png');
+}
+
+if(keypressed == '84'){
+    new_image('trunk.jpg');
+}
+
+if(keypressed == '82'){
+    new_image('roof.jpg');
+}
+
+if(keypressed == '89'){
+    new_image('yellow_wall.jpg');
+}
+
+if(keypressed == '68'){
+    new_image('dark_green.png');
+}
+
+if(keypressed == '85'){
+    new_image('unique.png');
+}
+
+if(keypressed == '67'){
+    new_image('cloud.jpg');
+}
+}
 
 
